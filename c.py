9627df8a1c8633e792e55fde36efd72e895e55b2
SonamTenzin1 <02230300.cst@rub.edu.bt>
2023-08-28 16:17:25 +0600

    hi

--- c.py
+++ c.py
@@ -4,5 +4,15 @@ print ("1.celsius to fahrenheit")
 print ("2.fahrenheit to celsius")
 print ("3.quit")
 
-tempvalue = int(input("please type the value and your "))
+choice = int(input("please enter your choice(1/2/3): "))
+
+if choice == 1:
+    celsius = float(input("please enter the temperature value in celsius: "))
+    fahrenheit = ((celsius* 9//5) + 32)
+    print(celsius,"°C is equal to ", fahrenheit, "°F")
+
+if choice == 2:
+    fahrenheit = float(input("please enter the temperature value in fahrenheit: "))
+    celsius = (fahrenheit - 32)*(5//9)
+    print(fahrenheit,"°F is equal to ", celsius, "°C")
 
