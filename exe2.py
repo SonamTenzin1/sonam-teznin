e57e8a6a5e2224485c84f931c118cecfd62579ff
SonamTenzin1 <02230300.cst@rub.edu.bt>
2023-09-12 19:20:39 +0600

    t

--- exe2.py
+++ exe2.py
@@ -11,3 +11,20 @@ def findMax(input):
     print(max)
 
 findMax(input)
+
+"""problem 2"""
+
+input_string = "(((((((((())"
+
+def findFloorAtEnd(input_string):
+    floor_at_end = 0
+
+    for bracket in input_string:
+        if bracket == '(':
+            floor_at_end += 1  # Go up one floor for an open bracket
+        elif bracket == ')':
+            floor_at_end -= 1  # Go down one floor for a close bracket
+
+    print('At the end, you will be at floor number:', floor_at_end)
+
+findFloorAtEnd(input_string)
